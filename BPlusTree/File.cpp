55a4382e204dc4059f73984c720ad1c1b50e0cbc
Shaoyi Zhang <zhangshaoyi1995@gmail.com>
2018-02-25 17:20:47 -0800

    add comments in FilePointer classl; modify gitignore

--- BPlusTree/File.cpp
+++ BPlusTree/File.cpp
@@ -1,5 +1,21 @@
 #include "File.h"
 
-FilePointer::FilePointer( const char * filename, lone int offset ) {
-  
+FilePointer::FilePointer( const char * filename, lone int offset, int nDupKeys ) {
+  this->filename = strdup( filename );
+  this->offset = offset;
+}
+
+FilePointer::FilePointer( string filename, long int offset, int nDupKeys ) {
+  FilePointer::FilePointer( filename.c_str(), offset, nDupKeys );
+}
+
+// TEST only 
+FilePointer::FilePointer( const char * filename, lone int offset, int nDupKeys, string word ) {
+  this->filename = strdup( filename );
+  this->offset = offset;
+  this->word = word;
+}
+
+FilePointer::FilePointer( string filename, long int offset, int nDupKeys, string word ) {
+  FilePointer::FilePointer( filename.c_str(), offset, nDupKeys, word );
 }
--- BPlusTree/File.h
+++ BPlusTree/File.h
@@ -1,27 +1,49 @@
 #ifndef FILE_H_
 #define FILE_H_
 
-/* A class for file operations
+/* A class for Record ID
   An instance of this class will include:
-  1. file name
-  2. offset of the target
-  
+  1. index file name ( index directory name in our project )
+  2. index page id
+  3. offset in the index page
+  4. In debugging phase, we will also store the keyword string in the object
+
   This class also contain some class methods to help retrieve actual records from
   the file given a target word. These method will take 
 */
 
+
+class FilePointer {
+public:
+  FilePointer( const char *filename, long int offset, int nDupKeys );
+  FilePointer( string filename, long int offset, int nDupKeys,  );
+  // Test only
+  FilePointer( const char *filename, long int offset, int nDupKeys, string word );
+  FilePointer( string filename, long int offset, int nDupKeys, string word );
+private:
+  char * filename; // index file name a.k.a. directory name for the index files
+  int pageID; // index page id
+  long int offset; // offset of the RID for the keyword
+
+  /* a keyword might appear multiple times in different documents. Our database will 
+  keep the RIDs of duplicated keywords in the same index page. Overflow pages will
+  be created automatically and will be transparent to the B Plus Tree. */
+  int nDupKeys;
+
+  
+  // NOTE: For debugging and test, we will add an extra filed: 
+  // the keyword associated with the RIDs.
+  // Remove in release to improve performance.
+  string word;
+};
+
+/* 
+  A wrapper class for file operations
+  might be unnecessary
+*/
 class FileOperation {
 
 };
 
-class FilePointer {
-public:
-  FilePointer( const char *filename, long int offset );
-  FilePointer( string filename, long int offset );
-private:
-  const char * filename; // the filename filed will be the same for all words in our project
-                         // however, it will be still useful for generic usage
-  long int offset;
-};
 
 #endif
