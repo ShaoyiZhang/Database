c90d89c38c1753cfa4d20e546e4ba83358fd5c7f
Shaoyi Zhang <zhangshaoyi1995@gmail.com>
2018-02-26 14:16:52 -0800

    move to linux

--- BPlusTree/File.cpp
+++ BPlusTree/File.cpp
@@ -1,6 +1,6 @@
 #include "File.h"
 
-FilePointer::FilePointer( const char * filename, lone int offset, int nDupKeys ) {
+FilePointer::FilePointer( const char * filename, long int offset, int nDupKeys ) {
   this->filename = strdup( filename );
   this->offset = offset;
 }
@@ -10,7 +10,7 @@ FilePointer::FilePointer( string filename, long int offset, int nDupKeys ) {
 }
 
 // TEST only 
-FilePointer::FilePointer( const char * filename, lone int offset, int nDupKeys, string word ) {
+FilePointer::FilePointer( const char * filename, long int offset, int nDupKeys, string word ) {
   this->filename = strdup( filename );
   this->offset = offset;
   this->word = word;
